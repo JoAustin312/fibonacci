7178a24d675cb7711af09e97990e9449c75af72d
JoAustin312 <65620601+JoAustin312@users.noreply.github.com>
2022-05-25 17:38:39 +0100

    Initial commit

---
+++ src/main/java/com/sparta/ja/fibonacci/Fibonacci.java
@@ -0,0 +1,27 @@
+package com.sparta.ja.fibonacci;
+
+import java.util.ArrayList;
+
+public class Fibonacci {
+
+    public static int getFibonacciNumberAtIndex(Integer indexInSequence) {
+
+        if (indexInSequence != null && indexInSequence >= 0){
+            if (indexInSequence == 1) return 0;
+            if (indexInSequence == 2) return 1;
+
+            return (getFibonacciNumberAtIndex(indexInSequence-1) + getFibonacciNumberAtIndex(indexInSequence-2));
+
+        }
+        return 0;
+    }
+
+    public static ArrayList<Integer> getFibonacciSequence(Integer sequenceLength){
+        ArrayList<Integer> arrayOfFibonacciNumbers = new ArrayList<>();
+        for (int i = 1; i <= sequenceLength; i++){
+            arrayOfFibonacciNumbers.add(getFibonacciNumberAtIndex(i));
+        }
+        return arrayOfFibonacciNumbers;
+    }
+
+}
---
+++ src/test/java/com/sparta/ja/fibonacci/FibonacciTest.java
@@ -0,0 +1,68 @@
+package com.sparta.ja.fibonacci;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FibonacciTest {
+
+    @Test
+    @DisplayName("check number returned is 0")
+    void checkNumberReturnedIs0(){
+        Assertions.assertEquals(0, Fibonacci.getFibonacciNumberAtIndex(1));
+    }
+
+    @Test
+    @DisplayName("check number returned is 1")
+    void checkNumberReturnedIs1(){
+        Assertions.assertEquals(1, Fibonacci.getFibonacciNumberAtIndex(2));
+    }
+
+    @Test
+    @DisplayName("check number returned is 2")
+    void checkNumberReturnedIs2(){
+        Assertions.assertEquals(1, Fibonacci.getFibonacciNumberAtIndex(3));
+    }
+
+    @Test
+    @DisplayName("check number returned is 3")
+    void checkNumberReturnedIs5(){
+        Assertions.assertEquals(2, Fibonacci.getFibonacciNumberAtIndex(4));
+    }
+
+    @Test
+    @DisplayName("check number returned is 377")
+    void checkNumberReturnedIs377(){
+        Assertions.assertEquals(377, Fibonacci.getFibonacciNumberAtIndex(15));
+    }
+
+    @Test
+    @DisplayName("check null value returns 0")
+    void checkNullValueReturns0(){
+        Assertions.assertEquals(0, Fibonacci.getFibonacciNumberAtIndex(null));
+    }
+
+    @Test
+    @DisplayName("check negative value returns 0")
+    void checkNegativeValueReturns0(){
+        Assertions.assertEquals(0, Fibonacci.getFibonacciNumberAtIndex(-1));
+    }
+
+    @Test
+    @DisplayName("return first 5 numbers in fibonacci")
+    void returnFirst5NumbersInFibonacci(){
+        ArrayList<Integer> testOne = new ArrayList<>(Arrays.asList(0, 1, 1, 2, 3));
+        Assertions.assertArrayEquals(testOne.toArray(), Fibonacci.getFibonacciSequence(5).toArray());
+    }
+
+    @Test
+    @DisplayName("return first 10 numbers in fibonacci")
+    void returnFirst10NumbersInFibonacci(){
+        ArrayList<Integer> testOne = new ArrayList<>(Arrays.asList(0, 1, 1, 2, 3, 5, 8, 13, 21, 34));
+        Assertions.assertArrayEquals(testOne.toArray(), Fibonacci.getFibonacciSequence(10).toArray());
+    }
+
+}
